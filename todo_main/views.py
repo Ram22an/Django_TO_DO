5825f517305c6b9a62ce2298e5ad1c72b41708fc
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-25 02:08:46 +0530

    added styling to the home.html page

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    tasks=Task.objects.all()
-    context={'tasks':tasks}
+    Completedtasks=Task.objects.filter(Is_completed=False)
+    context={'Completedtasks':Completedtasks}
     return render(request,'home/home.html',context)
 
