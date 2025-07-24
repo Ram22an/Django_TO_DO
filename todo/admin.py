38b87cf6bf518c90c5b63f2f357789b66d7e10a3
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-25 01:49:19 +0530

    customized the admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Task
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('task','Is_completed','created_at','updated_at')
+admin.site.register(Task,TaskAdmin)
