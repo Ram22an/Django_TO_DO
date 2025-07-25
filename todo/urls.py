90d24571411d0e535046e8d58222ab04d7346a39
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-26 01:24:40 +0530

    added marks as done functionality

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
+    path('mark_as_done/<int:id>',views.mark_as_done,name='mark_as_done'),
+    path('deleteTask/<int:id>',views.deleteTask,name='deleteTask'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from .models import Task
 # Create your views here.
 def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request,id):
+    task=get_object_or_404(Task,id=id)
+    task.Is_completed=True
+    task.save()
     return redirect('home')
 
