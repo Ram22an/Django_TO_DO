77852960ab3e2c364bf3feef7b886799a9a5def9
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-26 01:02:07 +0530

    added functionality to add task in app

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    Completedtasks=Task.objects.filter(Is_completed=False)
-    context={'Completedtasks':Completedtasks}
+    NotCompletedtasks=Task.objects.filter(Is_completed=False).order_by('updated_at')
+    Completedtasks=Task.objects.filter(Is_completed=True).order_by('updated_at')
+    context={'NotCompletedtasks':NotCompletedtasks,
+             'CompletedTasks':Completedtasks}
     return render(request,'home/home.html',context)
 
