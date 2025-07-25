f06e857a192dbab79d115a94a03dc3e5ab1b3f6b
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-26 01:40:29 +0530

    added undo task functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
     path('mark_as_done/<int:id>',views.mark_as_done,name='mark_as_done'),
     path('deleteTask/<int:id>',views.deleteTask,name='deleteTask'),
+    path('Mark_as_not_done/<int:id>',views.Mark_as_not_done,name='Mark_as_not_done'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -11,4 +11,13 @@ def mark_as_done(request,id):
     task.Is_completed=True
     task.save()
     return redirect('home')
+def deleteTask(request,id):
+    task=get_object_or_404(Task,id=id)
+    task.delete()
+    return redirect('home')
+def Mark_as_not_done(request,id):
+    task=get_object_or_404(Task,id=id)
+    task.Is_completed=False
+    task.save()
+    return redirect('home')
 
